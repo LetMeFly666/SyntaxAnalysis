52d306c2a75fb3da9add83c2c77f5e024499aaf6
LetMeFly666 <814114971@qq.com>
2022-06-21 12:30:12 +0800

    大小作业，吃饭吃饭

--- LSA.js
+++ LSA.js
@@ -2,11 +2,11 @@
  * @Author: LetMeFly
  * @Date: 2022-06-20 23:12:30
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-06-20 23:22:21
+ * @LastEditTime: 2022-06-21 12:29:58
  */
 // LetMeFly Syntax Analysis.js
 
-const samples = {  // 某种文法对应的样例
+const grammarSamples = {  // 某种文法对应的样例
     LL1: {
         grammar: "S -> ( L ) | a\nL -> L + S | L - S | R\nR -> a",
         string: "( a - ( a + a ) )"
@@ -28,3 +28,32 @@ const samples = {  // 某种文法对应的样例
         string: ""
     }
 };
+
+function getNT(grammars) {  // 获取终结符和非终结符
+    console.log(grammars);
+    const NT = {
+        N: new Set(),
+        T: new Set(),
+        error: new Set()
+    };
+    const NandT = [];
+    const lines = grammars.split("\n");
+    var lineNum = 0;
+    lines.forEach(line => {
+        lineNum++;
+        if (!line) {  // 空行
+            return;
+        }
+        const arrowSplited = line.split("->");
+        if (arrowSplited.length != 2) {
+            NT.error.push("Error: 第" + lineNum + "行，不只包含一个“->”");
+        }
+        const front = arrowSplited[0];
+        const back = arrowSplited[1];
+        const frontSplited = front.split(" ");
+        let N;
+        // frontSplited.forEach()
+    });
+
+    return NT;
+}
